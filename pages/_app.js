7774152680c46311bf974fc52b52dce15546e116
rakshith <rakshit.s.gowda@gmail.com>
2021-08-25 12:49:44 +0530

    register form toast notification

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,21 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Nav from '../components/Nav'
 import Head from 'next/head'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="/css/styles.css" />
+        <link rel='stylesheet' href='/css/styles.css' />
       </Head>
-      <Nav/>
+      <Nav />
+      <ToastContainer position='top-center' />
       <Component {...pageProps} />
     </>
   )
 }
-
 
 export default MyApp
--- pages/register.js
+++ pages/register.js
@@ -1,100 +1,109 @@
 import axios from 'axios'
 import { useState } from 'react'
-
-
+import { toast } from 'react-toastify'
 
 const Register = () => {
-const [name, setName] = useState('')
-const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
-const [secret, setSecret] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [secret, setSecret] = useState('')
+  const [ok, setOk] = useState(false)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     // check for name ,email,password,secret working
     // console.log(name, email, password, secret);
-    axios
-      .post('http://localhost:8000/api/register', {
+    try {
+      const { data } = await axios.post('http://localhost:8000/api/register', {
         name,
         email,
         password,
-        secret
-    })
-      .then((res) => console.log(res))
-      .catch(err => console.log(err))
+        secret,
+      })
+      setOk(data.ok)
+    } catch (err) {
+      toast.error(err.response.data)
+    }
   }
 
   return (
-    <div className="container-fluid">
-      <div className="row py-5 bg-secondary text-light">
-        <div className="col text-center">
+    <div className='container-fluid'>
+      <div className='row py-5 bg-secondary text-light'>
+        <div className='col text-center'>
           <h1>Register </h1>
         </div>
       </div>
-      <div className="row py-3">
-          <div className="col-md-6 offset-md-3">
-            <form  onSubmit={handleSubmit} >
-              <div className="form-group py-2"><small>
-                <label className="text-muted">Name</label></small>
+      <div className='row py-3'>
+        <div className='col-md-6 offset-md-3'>
+          <form onSubmit={handleSubmit}>
+            <div className='form-group py-2'>
+              <small>
+                <label className='text-muted'>Name</label>
+              </small>
               <input
-                  value={name}
-                  onChange={(e)=>setName(e.target.value)}
-                  type="text"
-                  className="form-control"
-                  placeholder='Enter Your Name'
-                  />
-              </div>
-            
-            
-              <div className="form-group py-2"><small>
-                <label className="text-muted">Email address </label></small>
-              <input
-                  value={email}
-                  onChange={(e)=>setEmail(e.target.value)}
-                  type="email"
-                  className="form-control"
-                  placeholder='Enter Your Email'
-                  />
-              </div>
-         
-            
-              <div className="form-group py-2"><small>
-                <label className="text-muted">Your Password</label></small>
-              <input
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  autoComplete="true"
-                  type="password"
-                  className="form-control"
-                  placeholder='Enter Password'
-                  />
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type='text'
+                className='form-control'
+                placeholder='Enter Your Name'
+              />
             </div>
 
-            <div className="form-group py-2"><small>
-              <label className="text-muted">Pick a question</label></small>
-              <select className="form-control">
-                <option >What is your favourite color ?</option>
-                <option >What is your best friend's name  ?</option>
-                <option >In which city were you born ?</option>
+            <div className='form-group py-2'>
+              <small>
+                <label className='text-muted'>Email address </label>
+              </small>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type='email'
+                className='form-control'
+                placeholder='Enter Your Email'
+              />
+            </div>
+
+            <div className='form-group py-2'>
+              <small>
+                <label className='text-muted'>Your Password</label>
+              </small>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete='true'
+                type='password'
+                className='form-control'
+                placeholder='Enter Password'
+              />
+            </div>
+
+            <div className='form-group py-2'>
+              <small>
+                <label className='text-muted'>Pick a question</label>
+              </small>
+              <select className='form-control'>
+                <option>What is your favourite color ?</option>
+                <option>What is your best friend's name ?</option>
+                <option>In which city were you born ?</option>
               </select>
-              <small className="form-text text-muted">
+              <small className='form-text text-muted'>
                 You can use this to reset your password if forgottten.
               </small>
             </div>
-            <div className="form-group py-2">
+            <div className='form-group py-2'>
               <input
                 value={secret}
                 onChange={(e) => setSecret(e.target.value)}
-                type="text"
-                placeholder="Write your Answer here"
-                className="form-control" />
+                type='text'
+                placeholder='Write your Answer here'
+                className='form-control'
+              />
             </div>
-            <div className="form-group d-grid py-3">
-               <button className="btn btn-primary btn-lg">Submit</button>
+            <div className='form-group d-grid py-3'>
+              <button className='btn btn-primary btn-lg'>Submit</button>
             </div>
-            </form>
-          </div>
+          </form>
         </div>
+      </div>
     </div>
   )
 }
