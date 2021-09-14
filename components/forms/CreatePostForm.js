c0fe125c0cc5856741bf0c33f51530b7fa06ae94
rakshith <rakshit.s.gowda@gmail.com>
2021-09-14 13:24:15 +0530

    create post -request

--- components/forms/CreatePostForm.js
+++ components/forms/CreatePostForm.js
@@ -1,4 +1,4 @@
-export default function CreatePostForm() {
+export default function CreatePostForm({ content, setContent, postSubmit }) {
   return (
     <div className='card'>
       <div className='card-body pb-3'>
@@ -6,15 +6,15 @@ export default function CreatePostForm() {
           <textarea
             className='form-control'
             placeholder='Write Something'
-            name=''
-            id=''
-            cols='30'
-            rows='10'
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           ></textarea>
         </form>
       </div>
       <div className='card-footer'>
-        <button className='btn btn-primary btn-sm mt-1'>Post</button>
+        <button onClick={postSubmit} className='btn btn-primary btn-sm mt-1'>
+          Post
+        </button>
       </div>
     </div>
   )
--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,9 +1,28 @@
-import { useContext } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/dist/client/router'
+import { useContext, useState } from 'react'
 import CreatePostForm from '../../components/forms/CreatePostForm'
 import UserRoute from '../../components/routes/UserRoute'
 import { UserContext } from '../../context'
 
 const Home = () => {
+  const [state, setState] = useContext(UserContext)
+  // state
+  const [content, setContent] = useState('')
+  // Route
+  const router = useRouter()
+
+  const postSubmit = async (e) => {
+    e.preventDefault()
+    // console.log('post=>', content)
+    try {
+      const { data } = axios.post('/create-post', { content })
+      console.log('create post response => ', data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <UserRoute>
       <div className='container-fluid'>
@@ -14,7 +33,11 @@ const Home = () => {
         </div>
         <div className='row py-3'>
           <div className='col-md-8'>
-            <CreatePostForm />
+            <CreatePostForm
+              content={content}
+              setContent={setContent}
+              postSubmit={postSubmit}
+            />
           </div>
           <div className='col-md-4'>{/* <Sidebar /> */} sidebar</div>
         </div>
