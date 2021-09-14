dc7b8e7a3795c0a0f368d62f3e4a86362e6a252e
rakshith <rakshit.s.gowda@gmail.com>
2021-09-14 09:28:29 +0530

    axios interceptor for user token expiree

---
+++ buildplan_05.md
@@ -0,0 +1,7 @@
+### have to handle expried token
+
+- after 7 days users token gets expired
+
+- use axios to configure interceptors globally
+
+- if there is error then lets Logout user
--- context/index.js
+++ context/index.js
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import { createContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/dist/client/router'
 
 const UserContext = createContext()
 
@@ -11,6 +13,24 @@ const UserProvider = ({ children }) => {
   useEffect(() => {
     setState(JSON.parse(window.localStorage.getItem('auth')))
   }, [])
+
+  const router = useRouter()
+  // Add a request interceptor
+  axios.interceptors.response.use(
+    function (response) {
+      // Do something before request is sent
+      return response
+    },
+    function (error) {
+      // Do something with request error
+      let res = error.response
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+        setState(null)
+        window.localStorage.removeItem('auth')
+        router.push('/login')
+      }
+    }
+  )
 
   return (
     <UserContext.Provider value={[state, setState]}>
